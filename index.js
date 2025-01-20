c14a290321f4d738a31f42c9d330a646433b9928
Tushar Dontulwar <150892030+TusharLOL@users.noreply.github.com>
2025-01-20 20:06:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 
 const corsOptions = {
-    origin: 'https://insta-clone-6vys.vercel.app',
+    origin: '*',
     credentials: true
 }
 app.use(cors(corsOptions));
